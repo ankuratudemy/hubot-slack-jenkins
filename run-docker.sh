b5209953120df62600b3ea5f8e164d18d47a3e98
Ankur Gupta <ankuratudemy@gmail.com>
2020-10-12 15:04:50 +0530

    removed old coffee scripts

--- run-docker.sh
+++ run-docker.sh
@@ -3,7 +3,7 @@
 # This is just an example on how we can run hubot with docker.
 export PORT=9999
 export EXPRESS_PORT=9999
-export HUBOT_SLACK_TOKEN=xoxb-1288639819079-1385505017584-gdiT8Hfc3zobZYZyTtLrk5pc
+export HUBOT_SLACK_TOKEN=xoxb-1288639819079-1385505017584-LujtALtENip9yJzaqSdS70vg
 export HUBOT_ADAPTER=slack
 export HUBOT_JENKINS_URL=http://jenkins-blueocean:8080/
 export HUBOT_JENKINS_AUTH=chatops:nhy6^YHNnhy6^YHN
--- run-local.sh
+++ run-local.sh
@@ -3,7 +3,7 @@
 # This is just an example on how we can run hubot locally.
 export PORT=9999
 export EXPRESS_PORT=9999
-export HUBOT_SLACK_TOKEN=xoxb-1288639819079-1385505017584-gdiT8Hfc3zobZYZyTtLrk5pc
+export HUBOT_SLACK_TOKEN=xoxb-1288639819079-1385505017584-LujtALtENip9yJzaqSdS70vg
 export HUBOT_ADAPTER=slack
 export HUBOT_JENKINS_URL=http://localhost:8080/
 export HUBOT_LOG_LEVEL=trace
